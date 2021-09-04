d929241c1f8a23cf68e08ff5f82efc70687af3e4
Fachri <fachrighiffary@gmail.com>
2021-09-05 02:12:53 +0700

    done build apk debug chat bot

--- App.js
+++ App.js
@@ -1,112 +1,178 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {Component} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  Image,
+  Keyboard,
 } from 'react-native';
+import {GiftedChat, InputToolbar, Send} from 'react-native-gifted-chat';
+import {Dialogflow_V2} from 'react-native-dialogflow';
+import {dialogflowConfig} from './env';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const avatarBot = require('./assets/images/avatar.png');
+const iconSend = require('./assets/images/sendicon.png');
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const BOT = {
+  _id: 2,
+  name: 'Mr. Bot',
+  avatar: avatarBot,
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+export class App extends Component {
+  state = {
+    keyboardStatus: undefined,
+    messages: [
+      {
+        _id: 1,
+        text: 'Hi my name is Mr.Robot',
+        createdAt: new Date(),
+        user: BOT,
+      },
+    ],
+    id: 1,
+    name: '',
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
+  componentDidMount = () => {
+    this.keyboardDidShowSubscription = Keyboard.addListener(
+      'keyboardDidShow',
+      () => {
+        this.setState({keyboardStatus: 'Keyboard Shown'});
+      },
+    );
+    this.keyboardDidHideSubscription = Keyboard.addListener(
+      'keyboardDidHide',
+      () => {
+        this.setState({keyboardStatus: 'Keyboard Hidden'});
+      },
+    );
+
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_ENGLISH_US,
+      dialogflowConfig.project_id,
+    );
+  };
+
+  componentWillUnmount() {
+    this.keyboardDidShowSubscription.remove();
+    this.keyboardDidHideSubscription.remove();
+  }
+
+  handleGoogleResponse = result => {
+    let text = result.queryResult.fulfillmentMessages[0].text.text[0];
+    this.sendBotResponse(text);
+  };
+
+  sendBotResponse = text => {
+    let msg;
+    if (text == 'senyum') {
+      msg = {
+        _id: this.state.messages.length + 1,
+        text: `Jangan lupa ${text} hari ini`,
+        image:
+          'https://scontent-sin6-4.xx.fbcdn.net/v/t1.18169-9/11205049_431366110393571_8723182005016167723_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHZ2EaXLLAKme75s1BnKOLbq7yJaFD-lRqrvIloUP6VGokAE5B4ZgfInqA_LtcmFuoY7z-kdF7H7rrQTgjqYEzg&_nc_ohc=nAshzIZway0AX8Hx7zq&_nc_ht=scontent-sin6-4.xx&oh=b0a9e16574131bf37826058f4753029a&oe=61585B01',
+        createdAt: new Date(),
+        user: BOT,
+      };
+    } else {
+      msg = {
+        _id: this.state.messages.length + 1,
+        text,
+        createdAt: new Date(),
+        user: BOT,
+      };
+    }
+
+    this.setState(previouseState => ({
+      messages: GiftedChat.append(this.state.messages, [msg]),
+    }));
+  };
+
+  onSend = (messages = []) => {
+    this.setState(previouseState => ({
+      messages: GiftedChat.append(previouseState.messages, messages),
+    }));
+    let message = messages[0].text;
+
+    Dialogflow_V2.requestQuery(
+      message,
+      result => this.handleGoogleResponse(result),
+      error => console.log(error),
+    );
+  };
+
+  onQuickReply = quickReply => {
+    this.setState(previouseState => ({
+      messages: GiftedChat.append(previouseState.messages, quickReply),
+    }));
+    console.log(quickReply[0]);
+    let message = quickReply[0].value;
+
+    Dialogflow_V2.requestQuery(
+      message,
+      result => this.handleGoogleResponse(result),
+      error => console.log(error),
+    );
+  };
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
         <View
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            height: 50,
+            width: '100%',
+            backgroundColor: 'lightblue',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Text style={{fontSize: 24, fontWeight: 'bold'}}>Chat Bot</Text>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+        <GiftedChat
+          isCustomViewBottom={true}
+          alwaysShowSend={true}
+          alignTop
+          renderSend={props => {
+            return (
+              <Send {...props}>
+                <View style={{marginHorizontal: 10, marginTop: 10}}>
+                  <Image source={iconSend} style={{height: 40, width: 40}} />
+                </View>
+              </Send>
+            );
+          }}
+          renderInputToolbar={props => {
+            return (
+              <InputToolbar
+                {...props}
+                style={{backgroundColor: 'red', marginTop: 15}}
+                containerStyle={{
+                  marginHorizontal: 15,
+                  marginBottom: 10,
+                  borderRadius: 10,
+                  backgroundColor: 'lightblue',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                }}
+              />
+            );
+          }}
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          onQuickReply={quickReply => this.onQuickReply(quickReply)}
+          user={{_id: 1}}
+        />
+      </SafeAreaView>
+    );
+  }
+}
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {flex: 1},
+});
---
+++ env.js
@@ -0,0 +1,14 @@
+export const dialogflowConfig = {
+  type: 'service_account',
+  project_id: 'chatbot-test-kpwc',
+  private_key_id: '2d388d9fa673fb17865a9120a2c427ab55671075',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCvGpUnEZ1EVrYi\nVMiT0846haP67rZdE9uv8SQazGvtfmWfO8zly9C6oA+5D65Di4ujVNMX6170GSkB\na2ZReU5BUiMxaivM4cRzsdw6y6xC6AVn88MhcsZETCcJQYUUciVVU+/x2ATzJcc4\nKmTS1TZ7T4Uc+Mrb2n06pkMpKeGSXTkaNvDAN/Rp5U2sBsrhYNpf8XH376A7EGEW\nmOqrkNd7I7u16Yb9Q2wkE4brN3+89UqglYbVZooPDgjku9xy/xIkpwjXhREEUgLy\nMQzayetjYNrmnLSYDvABS76fR+NHqvPRkUFYwOVo/xqXGdq/pEzW1/nDMdVXFqZH\noPzTGW6vAgMBAAECggEABkcIMNnfTvti1TE/7ZjKzGtNGSpkrEqRdinJdxJ8DgwZ\nfY9pxZU8euxRE/vdbft7OpRIaxObmFmskCn3BZW5OftFWripMFOptNIcCJmTp+Rk\niTkCdlkEhZ9LSPgp2rVK5Fe5guyRJNxmW3udmLdfGCWeKo86yo33LlqWjsu7FAGn\nOgEh9zyvH1YQXfR9VGGP8iyRJRdG+ZnEDeenY9rxnCAhpb2rqOvt2brO8fYEvs4u\nHyu5ZaiazhyjSJ3r/NPf4eP9RnegiocUAqoYz5GTQJN4ThQeTnAqhgd+0zQhA52l\naL8iNHSeCCn8EzhgSvTxWlOBAOKlsnwS/HWybHaA5QKBgQD1EWS9GrO+v2nDAcDz\nCZh/LitOWGf2yGkZ9yarXF1Et0fJL0aAXggcrY/9a5YqCNbFVcb2xGv2cG4KtbLS\nlVXMdz8wWoPFzGUsFGdprLzsxOPEdRtZovDNsqdd4eiBEts5WstGMgzIW71U4xKt\nUIJYdO28xZN6LtO2zw9VgLPqXQKBgQC26jgHCrkt34aGQA2WzuoLI1XgUtS+U67H\ndVaKG+ZTLgOGO/cLWghlkHu6On/cnUzkDHHGxtTvZO/tDKLgtN73XHZ7jPptr+5O\nXksjaDFoE0TSuEhh0NZVfy5x/PSUaO5X3g/qTqgTKQotTR8JxKPa20+yrcoYIgVu\nWbaoctHkewKBgA8OBHxBea1MIPkj0HCdtEqL0Yk8kRZt9VbtAekhAXHJOJW5W8Fj\nBWiNmPzecRkf9eGlhVV/d4pqGbh6tQOOvPiUksMrrFCoX8a3cirhu7Fn3v4gm4+E\nHPrl8LYpvJjBScpaM4aaaIKecCEY2frfqUI53KjuEkM9QTBHxA9imqrZAoGAGrDE\nqipjaREz1QUltnrM5gEq98e+aWNWnp49mXuGuV5+fyji3JIvpXpWw47jt3JwlNpI\nA+Rhj+kfpWxxm+5olpm0ZETnCfhhAqh5uFns0630HfdsAHHzxJgBU/rASaOiG8o4\nck01803KwV9+hvNdoYLc90qqsajNOBP5Nk1C2V8CgYEA4Uai8F40jGWwdUEkEcAG\nhkxGLHgJchMd7yCQ+RnfL8+v+D9b+GpwYirRh22nKMUWsDQTsZ4job9+NqKgjhwx\nsfMh2jnVmaUmLXr0KvMS6ohoCSBZezhlYuBvrWkUTvq1eiER2YOVvFEsgqVrJ13d\nwm/BNwjrTT0QCuhwnG7DXRc=\n-----END PRIVATE KEY-----\n',
+  client_email: 'chatbot-test@chatbot-test-kpwc.iam.gserviceaccount.com',
+  client_id: '116312617876151459366',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/chatbot-test%40chatbot-test-kpwc.iam.gserviceaccount.com',
+};
